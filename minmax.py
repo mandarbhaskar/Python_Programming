003d0adffe6d2e1f2701768211f6a522acc9e5ea
Mandar Bhaskar <141809770+mandarbhaskar@users.noreply.github.com>
2023-08-09 12:47:45 +0530

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,2 @@
+print("Minimum of 4,12,43.3,19 and GeeksforGeeks is : ",end="")
+print (min( 4,12,43.3,19,"GeeksforGeeks" ) )
---
+++ string.py
@@ -0,0 +1,25 @@
+a="Hello"
+print(a)
+
+a="""hey !
+         I am Mandar how can i help you
+         :)
+         """
+print(a)
+
+a="Hello ,World !"
+
+print(a)
+
+a="Hello , World!"
+
+print(len(a))
+
+print(a[:5])
+print(a[2:5])
+print(a[2:])
+print(a[-5:-2])
+
+print(a.upper())
+print(a.lower())
+print(a.replace("E","j"))
